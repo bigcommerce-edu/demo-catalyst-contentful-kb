721391404c667263c19dfd897baffff3f8d03905
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-05-09 16:34:54 -0500

    First iteration of fetching article content client-side

--- core/components/custom/knowledgebase/article.tsx
+++ core/components/custom/knowledgebase/article.tsx
@@ -1,11 +1,22 @@
+import { KbArticleFragment } from "~/app/api/kb/[locale]/article/[id]/route-data";
+import { FragmentOf } from "gql.tada";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { Document } from '@contentful/rich-text-types';
+
 interface ArticleProps {
-  id?: string;
+  article: FragmentOf<typeof KbArticleFragment>;
 }
 
 export function Article({
-  id
+  article
 }: ArticleProps) {
   return (
-    <div>{id ?? 'No id'}</div>
+    <div>
+      <div>ID: {article.sys.id}</div>
+      <div>Title: {article.title}</div>
+      <div className="prose">
+        {documentToReactComponents(article.body?.json as Document)}
+      </div>
+    </div>
   );
 }
