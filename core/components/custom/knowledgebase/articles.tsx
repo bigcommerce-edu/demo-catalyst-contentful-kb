23667bdba87a0775bb8b48b8c018df9c3bb2f487
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-07-18 17:13:32 -0500

    First steps for card implementation in Articles component

--- core/components/custom/knowledgebase/articles.tsx
+++ core/components/custom/knowledgebase/articles.tsx
@@ -1,7 +1,10 @@
+import { clsx } from 'clsx';
+
 import { KbArticlesItemFragment } from "~/app/api/kb/[locale]/list/route-data";
 import { FragmentOf } from "gql.tada";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { Document } from '@contentful/rich-text-types';
+import { Card } from '@/vibes/soul/primitives/card';
 
 interface KbArticlesProps {
   articles: Array<FragmentOf<typeof KbArticlesItemFragment>>;
@@ -11,16 +14,16 @@ export function KbArticles({
   articles
 }: KbArticlesProps) {
   return (
-    <ul>
+    <div className={clsx('w-full @container')}>
+      <div className="mx-auto grid grid-cols-1 gap-x-4 gap-y-6 @sm:grid-cols-2 @2xl:grid-cols-3 @2xl:gap-x-5 @2xl:gap-y-8 @5xl:grid-cols-4 @7xl:grid-cols-5">
       {articles.map(article => (
-        <li key={article.sys.id}>
-          <div>ID: {article.sys.id}</div>
-          <div>Title: {article.title}</div>
-          <div className="prose">
-            {documentToReactComponents(article.abstract?.json as Document)}
-          </div>
-        </li>
+        <Card
+          key={article.sys.id}
+          title={article.title ?? ''}
+          href=""
+        />
       ))}
-    </ul>
+      </div>
+    </div>
   );
 }
